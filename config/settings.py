7ae5aa3898732117c40fc3e0745e6fe83f2ab227
bernales <bernales@users.noreply.github.com>
2021-05-15 14:45:30 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -123,9 +123,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-DEFAULT_FROM_EMAIL = 'your_custom_email_account'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = 'sendgrid_password'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
